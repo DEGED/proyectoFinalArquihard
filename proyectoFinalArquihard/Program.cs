dfd5137cdcd4360f656e9be58d0644eae27e8530
DEGED <47962123+DEGED@users.noreply.github.com>
2021-06-03 16:33:29 -0500

    Tiene los tiempos por imagen, falta que sea tambien por version

--- proyectoFinalArquihard/Program.cs
+++ proyectoFinalArquihard/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace proyectoFinalArquihard
 {
@@ -25,12 +26,13 @@ namespace proyectoFinalArquihard
         {
             kernel = new sbyte[][] { new sbyte[] {0, 1, 0} , new sbyte[] { 1, 4, 1 }, new sbyte[] { 0, 1, 0 } };
         }
-        static byte[] filteringAlgorithmV1(byte[] data)
+        static byte[] filteringAlgorithmV1(byte[] data, int iv)
         {
             int offset = 1078;
             int n = (int)Math.Sqrt(data.Length - offset);
             byte[,] C = new byte[n, n];
             long time = DateTime.Now.Ticks;
+
             for (int x = 1; x < n - 1; x++) // starts at offset + n because image upper limit of the image is ignored
             {
                 for (int y = 1; y < n - 1; y++) // starts at 1 because
@@ -58,7 +60,7 @@ namespace proyectoFinalArquihard
                     co++;
                 }
             }
-
+            guardarTimes(finalTime+"", iv);
             return data;
         }
 
@@ -68,6 +70,7 @@ namespace proyectoFinalArquihard
             int n = (int)Math.Sqrt(data.Length - offset);
             byte[,] C = new byte[n, n];
             long time = DateTime.Now.Ticks;
+
             for (int x = 1; x < n - 1; x++) // starts at offset + n because image upper limit of the image is ignored
             {
                 for (int y = 1; y < n - 1; y++) // starts at 1 because
@@ -104,11 +107,12 @@ namespace proyectoFinalArquihard
             int offset = 1078;
             int n = (int)Math.Sqrt(data.Length - offset);
             byte[,] C = new byte[n, n];
-
             long time = DateTime.Now.Ticks;
+            int maximo = offset + n * (n - 1) + 1 - n;
+
             for (int y = 1; y < n - 1; y++) // starts at offset + n because image upper limit of the image is ignored
             {
-                for (int x = 1; x < n - 1; x++) // starts at 1 because
+                for (int x = 1; x < n - 1; x++) // starts at offset + n because image upper limit of the image is ignored
                 {
                     for (int i = 0; i < SIZE; i++)
                     {
@@ -124,6 +128,8 @@ namespace proyectoFinalArquihard
             }
             long time2 = DateTime.Now.Ticks;
             long finalTime = (time2 - time) / 100;
+
+
 
             int co = 0;
             for (int x = 0; x < n; x++) // starts at offset + n because image upper limit of the image is ignored
@@ -145,9 +151,11 @@ namespace proyectoFinalArquihard
             byte[,] C = new byte[n, n];
 
             long time = DateTime.Now.Ticks;
+
+            int maximo = offset + n * (n - 1) + 1 - n;
             for (int y = 1; y < n - 1; y++) // starts at offset + n because image upper limit of the image is ignored
             {
-                for (int x = 1; x < n - 1; x++) // starts at 1 because
+                for (int x = 1; x < n - 1; x++) // starts at offset + n because image upper limit of the image is ignored
                 {
                     for (int j = 0; j < SIZE; j++)
                     {
@@ -190,13 +198,65 @@ namespace proyectoFinalArquihard
             {
                 fs.Read(data, 0, data.Length);
             }
-            byte[] imagenConFiltro = filteringAlgorithmV1(data);
+            byte[] imagenConFiltro1 = filteringAlgorithmV1(data, i);
             
-            FileInfo output = new FileInfo("../../../../Imagenes/"+i+"outPut.bmp");
+            FileInfo output = new FileInfo("../../../../Imagenes/"+i+"-V1-outPut.bmp");
             using (FileStream fs = output.OpenWrite())
             {
                 fs.Write(data, 0, data.Length);
             }
+            byte[] imagenConFiltro2 = filteringAlgorithmV2(data);
+
+            FileInfo output2 = new FileInfo("../../../../Imagenes/" + i + "-V2-outPut.bmp");
+            using (FileStream fs = output2.OpenWrite())
+            {
+                fs.Write(data, 0, data.Length);
+            }
+
+            byte[] imagenConFiltro3 = filteringAlgorithmV3(data);
+
+            FileInfo output3 = new FileInfo("../../../../Imagenes/" + i + "-V3-outPut.bmp");
+            using (FileStream fs = output3.OpenWrite())
+            {
+                fs.Write(data, 0, data.Length);
+            }
+            byte[] imagenConFiltro4 = filteringAlgorithmV4(data);
+
+            FileInfo output4 = new FileInfo("../../../../Imagenes/" + i + "-V4-outPut.bmp");
+            using (FileStream fs = output4.OpenWrite())
+            {
+                fs.Write(data, 0, data.Length);
+            }
+        }
+
+        static void guardarTimes(String times, int i)
+        {
+            string path = @"../../../../image"+i+".txt";
+            try
+            {
+                // Create the file, or overwrite if the file exists.
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes(times);
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+
+                // Open the stream and read it back.
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (IOException ioe)
+            {
+                Console.WriteLine("FFFF");
+            }
         }
     }
+
 }
